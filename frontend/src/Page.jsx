0b99fadbd5c7bcff0c712efb2bff086a17ef5b44
d0Nt <deiviszxc@gmail.com>
2018-05-03 16:23:05 +0300

    Added content jsx file.

--- frontend/src/Page.jsx
+++ frontend/src/Page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header.jsx';
+import Content from './components/Content.jsx';
 
 export default class Page extends React.Component {
     constructor(props) {
@@ -9,6 +10,7 @@ export default class Page extends React.Component {
         return (
             <div>
                 <Header />
+                <Content />
             </div>
         );
     }
